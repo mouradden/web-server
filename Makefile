d2035224fef163487a3614fd51e23a2030a7e493
Mourad Denguir <denguirmourad0@gmail.com>
2024-02-01 14:57:28 +0100

    it seems that the server works

---
+++ Makefile
@@ -0,0 +1,27 @@
+SRCS = main.cpp Server.cpp
+
+OBJS = $(SRCS:.cpp=.o)
+
+NAME = webserv
+
+CPP = c++
+
+FLAGS = -Wall -Wextra -Werror -std=c++98
+
+all : $(NAME)
+
+$(NAME) : $(OBJS)
+	$(CPP) $(FLAGS) $(OBJS) -o $(NAME)
+
+%.o:%.cpp Server.hpp
+	$(CPP) $(FLAGS) -c $< -o $@ 
+
+clean:
+	rm -rf $(OBJS)
+
+fclean: clean
+	rm -rf $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include "Server.hpp"
-
+#include <vector>
 // cretae a socket
 // bind the socket to IP / port
 // mark the socket for listening
@@ -16,11 +16,12 @@ int main()
     server.createServer();
     server.putServerOnListening();
 
+    std::vector<int> activeConnections;
     fd_set setOfFds, readSet;
     FD_ZERO(&setOfFds);
     FD_SET(server.getServerSocket(), &setOfFds);
-
     int max_fd = server.getServerSocket();
+ 
 
     while (true) 
     {
@@ -28,63 +29,83 @@ int main()
 
         if (select(max_fd + 1, &readSet, NULL, NULL, NULL) == -1)
         {
-            std::cerr << "select";
-            exit(1);
+            std::cerr << "Error in select\n";
+            return 1;
         }
         if (FD_ISSET(server.getServerSocket(), &readSet))
         {
-            sockaddr_in clientAddress;
-            socklen_t clientAddressSize = sizeof(clientAddress);
-            int clientSocket = accept(server.getServerSocket(), reinterpret_cast<sockaddr*>(&clientAddress), &clientAddressSize); // return the new socket fd for the incoming client connection
-            if (clientSocket != -1)
+            int clientSocket = accept(server.getServerSocket(), NULL, NULL);
+            if (clientSocket == -1)
             {
-                FD_SET(clientSocket, &setOfFds); // add to global set
-                if (clientSocket > max_fd)   // update the max
-                {
+                std::cerr << "Error accepting client connection\n";
+            }
+            else
+            {
+                std::cout << "New client connected, socket: " << clientSocket << "\n";
+                if (clientSocket > max_fd)
                     max_fd = clientSocket;
-                }
-                std::cout << "New connection from " << inet_ntoa(clientAddress.sin_addr) << " on socket " << clientSocket << "\n"  ;
-
-                
+                activeConnections.push_back(clientSocket);
+                FD_SET(clientSocket, &setOfFds);
             }
         }
-        for(int fd = 0; fd <= max_fd; fd++)
-        {
-            if (FD_ISSET(fd, &readSet)) // fd is ready for reading
+            for(std::vector<int>::iterator it = activeConnections.begin(); it != activeConnections.end(); ++it)
             {
-                char buffer[4096];
-                memset(buffer, 0, 4096);
-                ssize_t bytesRead = recv(fd, buffer, 4096 - 1, 0);
-                if (bytesRead > 0)
+                if (FD_ISSET(*it, &readSet))
                 {
-                    std::cout << "Received request:\n" << buffer << std::endl;
-                    std::ifstream file("index.html");
-                    if (!file)
+                    char buffer[4096];
+                    memset(buffer, 0, 4096);
+                    ssize_t bytesRead = recv(*it, buffer, 4096 - 1, 0); //receive request
+                    if (bytesRead > 0)
                     {
-                        std::cerr << "Error opening file" << std::endl;
-                        return 1;
+                        std::cout << "Socket : " << *it << " Received request:\n" << buffer << std::endl;
+                        // example how to handle a request
+                        std::string request(buffer);
+                        std::string method = request.substr(0, request.find(" "));
+                        std::cout << "HTTP method: " << method << std::endl;
+                        if (method == "GET")
+                        {
+                            size_t firstSpace = request.find(" ");
+                            size_t secondSpace = request.find(" ", firstSpace + 1);
+                            std::string target = request.substr(firstSpace + 1, secondSpace - firstSpace - 1);
+                            std::cout << "---------->|" << target << "|\n";
+                            if (target == "/")
+                            {
+                                std::ostringstream ss;
+                                std::ifstream file("index.html");
+                                if (!file)
+                                {
+                                    std::cerr << "Error opening file" << std::endl;
+                                    return 1;
+                                }
+                                ss << file.rdbuf();
+                                std::string file_contents = ss.str();
+                                // // Prepare the HTTP response
+                                std::ostringstream response;
+                                // std::cout << "file_contents : " << file_contents << std::endl;
+                                response << "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " << file_contents.size() << "\r\n\r\n" << file_contents;
+                                // Send the HTTP response
+                                send(*it, response.str().c_str(), response.str().size(), 0);
+                                // close(*it);
+                            }
+                        }
                     }
-                    // Read the file into a string
-                    std::ostringstream ss;
-                    ss << file.rdbuf();
-                    std::string file_contents = ss.str();
-                    // Prepare the HTTP response
-                    std::ostringstream response;
-                    response << "HTTP/1.1 200 OK\r\nContent-Length: " << file_contents.size() << "\r\n\r\n" << file_contents;
-                    // Send the HTTP response
-                    send(fd, response.str().c_str(), response.str().size(), 0);
-                }
-                else if (bytesRead == 0)
-                {
-                    std::cout << "Client closed connection" << std::endl;
-                }
-                else
-                {
-                    std::cerr << "Error reading from socket" << std::endl;
-                    close(fd);
-                    FD_CLR(fd, &setOfFds); // remove from master set
+                    else if (bytesRead == 0)
+                    {
+                        // Connection closed by the client
+                        std::cout << "Client disconnected, socket: " << *it << "\n";
+                        close(*it);
+                        FD_CLR(*it, &setOfFds);
+                        it = activeConnections.erase(it) - 1; // Decrement iterator to avoid skipping the next element
+                    }
+                    else
+                    {
+                        // Error in recv
+                        std::cerr << "Error receiving data from client, socket: " << *it << "\n";
+                        close(*it);
+                        FD_CLR(*it, &setOfFds);
+                        it = activeConnections.erase(it) - 1;
+                    }
                 }
             }
-        }
     }
 }
